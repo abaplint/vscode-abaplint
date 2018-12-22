9d13ebb5a00723671346050a28cdfc303cf74178
larshp <larshp@hotmail.com>
2018-12-22 10:10:45 +0100

    handle multiple folders

--- server/src/handler.ts
+++ server/src/handler.ts
@@ -6,15 +6,21 @@ import {WorkspaceFolder} from "vscode-languageserver";
 import * as abaplint from "abaplint";
 import {Registry} from "abaplint/build/src/registry"; // todo, typing?
 
+interface IFolder {
+  root: string;
+  dotabapgit: string | undefined;
+  starting: string;
+}
+
 export class Handler {
-  private folders: WorkspaceFolder[] | null;
+  private folders: IFolder[] = [];
   private reg: Registry;
   private connection: LServer.Connection;
 
   constructor(connection: LServer.Connection, params: LServer.InitializeParams) {
     this.reg = new abaplint.Registry();
-    this.folders = params.workspaceFolders;
     this.connection = connection;
+    this.setFolders(params.workspaceFolders);
     this.setConfig();
 // todo, load all files from folders
 // todo, also XML files should be loaded into registry
@@ -57,21 +63,47 @@ export class Handler {
     this.connection.sendDiagnostics({uri: textDocument.uri, diagnostics});
   }
 
-  private setConfig() {
-    if (this.folders) {
-      for (const folder of this.folders) {
-        this.connection.console.log(folder.name + " " + folder.uri);
+  private setFolders(workspaces: WorkspaceFolder[] | null) {
+    if (workspaces) {
+      for (const workspace of workspaces) {
+        this.folders.push({
+          root: workspace.uri,
+          dotabapgit: undefined,
+          starting: workspace.uri});
       }
     }
+
+    for (const folder of this.folders) {
+      try {
+        const name = folder.root + path.sep + ".abapgit.xml";
+        const xml = fs.readFileSync(name, "utf-8");
+        folder.dotabapgit = name;
+
+        const result = xml.match(/<STARTING_FOLDER>(\d{2})<\/STARTING_FOLDER>/);
+        if (result) {
+          folder.starting = folder.root + result[1];
+        }
+      } catch {
+        this.connection.console.log("no .abapgit.xml found, " + folder.root);
+      }
+    }
+
+    this.connection.console.log("Folder overview:");
+    for (const folder of this.folders) {
+      this.connection.console.log(folder.root + " " + folder.dotabapgit + " " + folder.starting);
+    }
+  }
+
+  private setConfig() {
 // todo, multi folder vs config?
     try {
-      if (this.folders) {
-        const raw = fs.readFileSync(this.folders[0].uri + path.sep + "abaplint.json", "utf-8"); // todo, rootPath is deprecated
+      if (this.folders.length > 0) {
+        const raw = fs.readFileSync(this.folders[0].root + path.sep + "abaplint.json", "utf-8"); // todo, rootPath is deprecated
         const config = new abaplint.Config(raw);
         this.reg.setConfig(config);
         this.connection.console.log("custom abaplint.json found");
       }
-    } catch (err) {
+    } catch {
       this.connection.console.log("no custom abaplint config, using defaults");
     }
   }
