b87f6369e85b7c198988bae2b2cdad6c0ab9cd6b
Marcello Urbani <marcello.urbani@gmail.com>
2020-08-13 08:57:52 +0100

    extract fs interface

--- server/src/file_operations.ts
+++ server/src/file_operations.ts
@@ -1,26 +1,51 @@
-import * as fs from "fs";
+import {PathLike, Stats,exists} from "fs";
+import {readFile,lstat,unlink,rmdir,readdir} from "fs/promises";
 import * as path from "path";
 import * as glob from "glob";
 import {IFile, MemoryFile} from "@abaplint/core";
+import {promisify} from "util";
+
+interface FsProvider{
+  readFile:(path:PathLike,options:{ encoding: BufferEncoding; flag?: string; } )=>Promise<string>
+  exists:(path:PathLike)=>Promise<boolean>
+  lstat:(path: PathLike)=> Promise<Stats>
+  unlink:(path: PathLike)=> Promise<void>
+  readdir:(path: PathLike)=> Promise<string[]>
+  rmdir:(path: PathLike)=> Promise<void>
+}
+
+let provider:FsProvider = {
+  readFile,
+  exists:promisify(exists),
+  lstat,
+  unlink,
+  rmdir,
+  readdir,
+};
+
+export function registerProvider(newProvider:FsProvider){
+  provider = newProvider;
+}
 
 export class FileOperations {
 
   public static async readFile(name: string): Promise<string> {
-    return fs.promises.readFile(name, "utf-8");
+    return provider.readFile(name,{encoding: "utf-8"});
   }
 
   public static async deleteFolderRecursive(p: string) {
-    if (fs.existsSync(p)) {
-      const files = fs.readdirSync(p);
+    if (await provider.exists(p)) {
+      const files = await provider.readdir(p);
       for (const file of files) {
         const curPath = p + path.sep + file;
-        if (fs.lstatSync(curPath).isDirectory()) {
+        const curStats = await provider.lstat(curPath);
+        if (curStats.isDirectory()) {
           await this.deleteFolderRecursive(curPath);
         } else {
-          fs.unlinkSync(curPath);
+          await provider.unlink(curPath);
         }
       }
-      fs.rmdirSync(p);
+      await provider.rmdir(p);
     }
   }
 
@@ -45,7 +70,7 @@ export class FileOperations {
 
       // note that readFileSync is typically faster than async readFile,
       // https://medium.com/@adamhooper/node-synchronous-code-runs-faster-than-asynchronous-code-b0553d5cf54e
-      const raw = fs.readFileSync(filename, "utf8");
+      const raw = await FileOperations.readFile(filename);
       files.push(new MemoryFile(filename, raw));
     }
     return files;
