5a6775544235197ecbacfd6fbf4d202be6f35860
Marcello Urbani <marcello.urbani@gmail.com>
2020-08-21 09:16:37 +0100

    better initialization

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,137 +4,158 @@ import {Handler} from "./handler";
 import {registerProvider} from "./fs_provider";
 
 const connection = LServer.createConnection(LServer.ProposedFeatures.all);
-let handler: Handler;
 
 // create a simple text document manager. The text document manager
 // supports full document sync only
 const documents = new LServer.TextDocuments(TextDocument);
 let hasConfigurationCapability: boolean | undefined = false;
 let hasWorkspaceFolderCapability: boolean | undefined = false;
-let createHandler = ()=>Promise.resolve();
 
-connection.onInitialize(async (params: LServer.InitializeParams, _cancel, progress) => {
+function initialize(){
 
-  const capabilities = params.capabilities;
-  const {provideFsProxy = false} = params.initializationOptions;
-  if(provideFsProxy) {registerProvider(connection);}
+  const handlerParams = new Promise<LServer.InitializeParams>((resolve,reject)=>{
+    connection.onInitialize(async (params: LServer.InitializeParams, _cancel) => {
+      try {
+        const capabilities = params.capabilities;
+        const {provideFsProxy = false} = params.initializationOptions;
+        if(provideFsProxy) {registerProvider(connection);}
 
-  // does the client support the `workspace/configuration` request?
-  // if not, we will fall back using global settings
-  hasConfigurationCapability =
-    capabilities.workspace && !!capabilities.workspace.configuration;
-  hasWorkspaceFolderCapability =
-    capabilities.workspace && !!capabilities.workspace.workspaceFolders;
+    // does the client support the `workspace/configuration` request?
+    // if not, we will fall back using global settings
+        hasConfigurationCapability =
+      capabilities.workspace && !!capabilities.workspace.configuration;
+        hasWorkspaceFolderCapability =
+      capabilities.workspace && !!capabilities.workspace.workspaceFolders;
 
-  createHandler = async ()=>{
+        const result: LServer.InitializeResult = {
+          capabilities: {
+      /*
+      signatureHelpProvider: [],
+      completionProvider
+      codeLensProvider
+      */
+            textDocumentSync: LServer.TextDocumentSyncKind.Full,
+            documentFormattingProvider: true,
+            definitionProvider: true,
+            codeActionProvider: true,
+            referencesProvider: true,
+            documentHighlightProvider: true,
+            documentSymbolProvider: true,
+            implementationProvider: true,
+            renameProvider: {prepareProvider: true},
+            hoverProvider: true,
+          }};
+        resolve(params);
+        return result;
+        //
+      } catch (error) {
+        reject(error);
+        throw(error);
+      }
+    });
+
+
+  });
+
+  const initialized = new Promise((resolve,reject)=>{
+    connection.onInitialized(async () => {
+      try {
+        if (hasConfigurationCapability) {
+          connection.client.register(
+            LServer.DidChangeConfigurationNotification.type,
+            undefined);
+        }
+        if (hasWorkspaceFolderCapability) {
+          connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+          // todo, handle event
+            connection.console.log("Workspace folder change event received.");
+          });
+        }
+        resolve(true);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+
+  let handler:Promise<Handler>;
+
+  const createHandler = async() =>{
+    await initialized;
+    const params = await handlerParams;
+    const progress = await connection.window.createWorkDoneProgress();
     progress.begin("", 0, "Initialize", true);
-    handler = await Handler.create(connection, params);
+    const handler = await Handler.create(connection, params);
     await handler.loadAndParseAll(progress);
     progress.done();
+    handler.updateTooltip();
+    return handler;
   };
-  if(!provideFsProxy) {await createHandler();}
-  // await createHandler();
 
-  const result: LServer.InitializeResult = {capabilities: {
-    /*
-    signatureHelpProvider: [],
-    completionProvider
-    codeLensProvider
-    */
-    textDocumentSync: LServer.TextDocumentSyncKind.Full,
-    documentFormattingProvider: true,
-    definitionProvider: true,
-    codeActionProvider: true,
-    referencesProvider: true,
-    documentHighlightProvider: true,
-    documentSymbolProvider: true,
-    implementationProvider: true,
-    renameProvider: {prepareProvider: true},
-    hoverProvider: true,
-  }};
+  return ()=>{
+    if(!handler){handler = createHandler();}
+    return handler;
+  };
+}
 
-  return result;
-});
-const initialized = new Promise((resolve,reject)=>{
-  connection.onInitialized(async () => {
-    try {
-      if(!handler){ await createHandler();}
-      handler.updateTooltip();
-
-      if (hasConfigurationCapability) {
-        connection.client.register(
-          LServer.DidChangeConfigurationNotification.type,
-          undefined);
-      }
-      if (hasWorkspaceFolderCapability) {
-        connection.workspace.onDidChangeWorkspaceFolders((_event) => {
-          // todo, handle event
-          connection.console.log("Workspace folder change event received.");
-        });
-      }
-      resolve(true);
-    } catch (error) {
-      reject(error);
-    }
-  });
-});
+const getHandler = initialize();
 
 connection.onCodeAction(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onCodeAction(params);
 });
 
 connection.onDocumentHighlight(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onDocumentHighlight(params);
 });
 
 connection.onDefinition(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onDefinition(params);
 });
 
 connection.onHover(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onHover(params);
 });
 
 connection.onRenameRequest(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onRename(params);
 });
 
 connection.onPrepareRename(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onPrepareRename(params);
 });
 
 connection.onDocumentFormatting(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onDocumentFormatting(params);
 });
 
 connection.onDocumentSymbol(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onDocumentSymbol(params);
 });
 
 connection.onDidChangeConfiguration((_change) => { return undefined; });
 
 documents.onDidClose(async (e) => {
-  await initialized;
   connection.sendDiagnostics({uri: e.document.uri, diagnostics: []});
 });
 
 documents.onDidChangeContent(async (change) => {
-  await initialized;
+  const handler = await getHandler();
   handler.validateDocument(change.document);
 });
 
 // todo, documents.onDelete, how are file deletions handled ?
 
-connection.onDidChangeWatchedFiles((_change) => {
+connection.onDidChangeWatchedFiles(async (_change) => {
   connection.console.log("We received a file change event");
+  const handler = await getHandler();
   for (const change of _change.changes.values()) {
     if (change.uri.endsWith("abaplint.json")) {
       connection.console.log("abaplint.json was changed, reloading...");
@@ -145,36 +166,37 @@ connection.onDidChangeWatchedFiles((_change) => {
 });
 
 connection.onImplementation(async(params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onImplementation(params);
 });
 
 connection.onReferences(async (params) => {
-  await initialized;
+  const handler = await getHandler();
   return handler.onReferences(params);
 });
 
-connection.onRequest("abaplint/help/request", (data) => {
+connection.onRequest("abaplint/help/request", async (data) => {
+  const handler = await getHandler();
   handler.onHelp(data.uri, data.position);
 });
 
 connection.onRequest("abaplint/highlight/definitions/request", async (data) => {
-  await initialized;
+  const handler = await getHandler();
   handler.onHighlightDefinitions(data);
 });
 
 connection.onRequest("abaplint/highlight/reads/request", async (data) => {
-  await initialized;
+  const handler = await getHandler();
   handler.onHighlightReads(data);
 });
 
 connection.onRequest("abaplint/highlight/writes/request", async (data) => {
-  await initialized;
+  const handler = await getHandler();
   handler.onHighlightWrites(data);
 });
 
 connection.onRequest("abaplint/config/default/request", async () => {
-  await initialized;
+  const handler = await getHandler();
   handler.onGetConfig();
 });
 
