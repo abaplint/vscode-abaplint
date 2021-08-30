ee1db3427a5be27ccc50f3a33693dd6b095355ed
larshp <larshp@hotmail.com>
2021-08-30 08:15:57 +0200

    add browser script

---
+++ browser.webpack.config.js
@@ -0,0 +1,92 @@
+//@ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+'use strict';
+
+const path = require('path');
+
+const browserClientConfig = /** @type WebpackConfig */ {
+	context: path.join(__dirname, 'client'),
+	mode: 'none',
+	target: 'webworker', // web extensions run in a webworker context
+	entry: {
+		client: './src/extension.ts',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, 'out-browser'),
+		libraryTarget: 'commonjs',
+	},
+	resolve: {
+		mainFields: ['module', 'main'],
+		extensions: ['.ts', '.js'], // support ts-files and js-files
+		alias: {},
+		fallback: {
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+					},
+				],
+			},
+		],
+	},
+	externals: {
+		vscode: 'commonjs vscode', // ignored because it doesn't exist
+	},
+	performance: {
+		hints: false,
+	},
+	devtool: 'source-map',
+};
+
+const browserServerConfig = /** @type WebpackConfig */ {
+	context: path.join(__dirname, 'server'),
+	mode: 'none',
+	target: 'webworker', // web extensions run in a webworker context
+	entry: {
+		server: './src/server.ts',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, 'out-browser'),
+		libraryTarget: 'var',
+		library: 'serverExportVar'
+	},
+	resolve: {
+		mainFields: ['module', 'main'],
+		extensions: ['.ts', '.js'], // support ts-files and js-files
+		alias: {},
+		fallback: {
+			//path: require.resolve("path-browserify")
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+					},
+				],
+			},
+		],
+	},
+	externals: {
+		vscode: 'commonjs vscode', // ignored because it doesn't exist
+	},
+	performance: {
+		hints: false,
+	},
+	devtool: 'source-map'
+};
+
+module.exports = [browserClientConfig, browserServerConfig];
