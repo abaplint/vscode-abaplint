d4fa4b2dc3d12e478d04d700c35584bbab525b47
Marcello Urbani <marcello.urbani@gmail.com>
2023-10-21 10:54:48 +0300

    generate dot files for graphs rather than a webview Fixes #150

--- client/src/flowProvider.ts
+++ client/src/flowProvider.ts
@@ -1,33 +1,42 @@
-import { EventEmitter, TextDocumentContentProvider, Uri, workspace } from "vscode"
+import {EventEmitter, TextDocumentContentProvider, Uri, workspace} from "vscode";
 
-export const ABAPLINTSCHEME = "ABAPLINT"
+export const ABAPLINTSCHEME = "ABAPLINT";
 
 class DocProvider implements TextDocumentContentProvider {
-  private static instance: DocProvider
-  private emitter= new EventEmitter<Uri>;
-  onDidChange = this.emitter.event
-  documents = new Map<string,string>()
-  async provideTextDocumentContent(uri: Uri) {
-    if (uri.scheme !== ABAPLINTSCHEME)
-      throw new Error(`Unexpected URI scheme ${uri.scheme}`)
+  private static instance: DocProvider;
+  private readonly emitter = new EventEmitter<Uri>();
+  public onDidChange = this.emitter.event;
+  private readonly documents = new Map<string, string>();
+  public async provideTextDocumentContent(uri: Uri) {
+    if (uri.scheme !== ABAPLINTSCHEME) {
+      throw new Error(`Unexpected URI scheme ${uri.scheme}`);
+    }
 
-    const contents = this.documents.get(uri.path)
-    if(typeof contents!== "string")throw new Error(`Unknown document ${uri.toString()}`)
-    return contents
+    const contents = this.documents.get(uri.path);
+    return contents || "#no content found";
   }
-  setDocument(uri: Uri,contents: string){
-    this.documents.set(uri.path,contents)
-    this.emitter.fire(uri)
+  public setDocument(uri: Uri, contents: string) {
+    if (contents) {
+      this.documents.set(uri.path, contents);
+      this.emitter.fire(uri);
+    } else {this.documents.delete(uri.path);}
   }
-  public static get(){
-    if(!DocProvider.instance) DocProvider.instance= new DocProvider()
-    return DocProvider.instance
-
+  public static get() {
+    if (!DocProvider.instance) {
+      DocProvider.instance = new DocProvider();
+    }
+    return DocProvider.instance;
   }
 }
 
-workspace.registerTextDocumentContentProvider(ABAPLINTSCHEME, DocProvider.get())
+workspace.registerTextDocumentContentProvider(ABAPLINTSCHEME, DocProvider.get());
 
-export const setDocument = (url: Uri,contents: string) => {
-    DocProvider.get().setDocument(url,contents)
-}
+export const setDocument = (url: Uri, contents: string) => {
+  DocProvider.get().setDocument(url, contents);
+};
+
+workspace.onDidCloseTextDocument((doc) => {
+  if (doc.uri.scheme === ABAPLINTSCHEME) {
+    DocProvider.get().setDocument(doc.uri, "");
+  }
+});
--- client/src/flows.ts
+++ client/src/flows.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 import {ExtensionContext} from "vscode";
 import {BaseLanguageClient} from "vscode-languageclient/node";
-import { ABAPLINTSCHEME, setDocument } from "./flowProvider";
+import {ABAPLINTSCHEME, setDocument} from "./flowProvider";
 
 export class Flows {
   // private panel: vscode.WebviewPanel | undefined;
@@ -16,53 +16,42 @@ export class Flows {
     return this;
   }
 
-  // private build(html: string): string {
-  //   return `<!DOCTYPE html>
-  //   <html lang="en">
-  //   <head>
-  //     <meta charset="UTF-8">
-  //     <meta http-equiv="Content-Security-Policy" content="default-src 'none';">
-  //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //     <title>abaplint flows</title>
-  //   </head>
-  //   <body>` + html + `</body>
-  //   </html>`;
-  // }
-
   public async flowResponse(response: string) {
-    const uri=vscode.Uri.parse(`${ABAPLINTSCHEME}:graph.dot`)
-    const contents= JSON.parse(response).join("\n\n");
-    setDocument(uri,contents)
-    const doc = await vscode.workspace.openTextDocument(uri); 
-    await vscode.window.showTextDocument(doc, { preview: false });
-    // if (this.panel) {
-    //   let list = JSON.parse(response) as string[];
-    //   list = list.map(l => "<pre>" + l + "</pre>");
-    //   this.panel.webview.html = this.build(list.join("<br><br>\n"));
-    // }
+    this.emitter.fire(response);
+    const uri = vscode.Uri.parse(`${ABAPLINTSCHEME}:graph.dot`);
+    const contents = JSON.parse(response).join("\n\n");
+    setDocument(uri, contents);
   }
 
-  public show() {
+  private readonly emitter = new vscode.EventEmitter<string>();
+  private pending?: Promise<void>;
+
+  public async show() {
     const editor = vscode.window.activeTextEditor;
     if (editor === undefined) {
       return;
     }
+    const path = editor.document.uri.path.replace(/\.[^\.]*/, "").replace(/.*\//, "");
+    const uri = vscode.Uri.parse(`${ABAPLINTSCHEME}:${path}.dot`);
+    setDocument(uri, "Loading");
+    const doc = await vscode.workspace.openTextDocument(uri);
+    await vscode.window.showTextDocument(doc, {preview: false, viewColumn: vscode.ViewColumn.Beside});
 
-    // if (this.panel === undefined) {
-    //   this.panel = vscode.window.createWebviewPanel(
-    //     "abaplint_flows",
-    //     "abaplint flows",
-    //     {viewColumn: vscode.ViewColumn.Beside, preserveFocus: true},
-    //     {enableFindWidget: true, enableCommandUris: true, enableScripts: true}
-    //   );
-    //   this.panel.onDidDispose(() => { this.panel = undefined; });
-    // } else {
-    //   this.panel.reveal(undefined, true);
-    // }
+    if (this.pending) {
+      await this.pending;
+    }
+    this.pending = new Promise((resolve) => {
+      this.emitter.event((content) => {
+        const contents = JSON.parse(content).join("\n\n");
+        setDocument(uri, contents);
+        this.pending = undefined;
+        resolve();
+      });
+    });
 
-    // this.panel.webview.html = this.build("loading");
-
-    this.client.sendRequest("abaplint/dumpstatementflows/request", {uri: editor.document.uri.toString(), position: editor.selection.active});
+    this.client.sendRequest("abaplint/dumpstatementflows/request", {
+      uri: editor.document.uri.toString(),
+      position: editor.selection.active,
+    });
   }
-
 }
